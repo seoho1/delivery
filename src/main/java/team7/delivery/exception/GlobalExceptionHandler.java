369ff8ee9f9fa8143a51aeb2a5270e06f1677a58
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-01-07 22:52:31 +0900

    Initial commit

---
+++ src/main/java/team7/delivery/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package team7.delivery.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Map<String, Object>> handleCustomException (CustomException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return getErrorResponse(status, e.getMessage);
+
+    }
+
+    public ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", status.name());
+        errorResponse.put("code", status.value());
+        errorResponse.put("message", message);
+
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+}
---
+++ src/test/java/team7/delivery/DeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package team7.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
