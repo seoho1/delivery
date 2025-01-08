2d551eb15949a699aa4a706d7677e5ed1a9e2562
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-01-08 16:38:19 +0900

    feat: add validation of email exist 이메일이 존재하는지에 대한 검증 추가, UserRepository 에 findByEmail 메서드 추가.

--- src/main/java/team7/delivery/service/UserService.java
+++ src/main/java/team7/delivery/service/UserService.java
@@ -1,9 +1,11 @@
 package team7.delivery.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import team7.delivery.dto.user.UserCreateResponseDto;
 import team7.delivery.entity.User;
+import team7.delivery.exception.CustomException;
 import team7.delivery.repository.UserRepository;
 
 @Service
@@ -12,13 +14,25 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserCreateResponseDto createUser(String email, String password){
+    public UserCreateResponseDto createUser(String email, String password) {
+
+        checkRegisteredUser(email);
 
         User user = User.of(email, password);
 
         User savedUser = userRepository.save(user);
 
         return UserCreateResponseDto.of(savedUser);
+    }
+
+    public void checkRegisteredUser(String email) {
+        User foundUser = userRepository.findByEmail(email).orElse(null);
+
+        if (foundUser == null) {
+            return;
+        }
+
+        throw new CustomException(HttpStatus.BAD_REQUEST, "등록하려는 이메일이 이미 존재합니다");
     }
 
 }
