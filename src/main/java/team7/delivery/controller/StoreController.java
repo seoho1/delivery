9ae867ce226420ae64613fb120f0ca4e50b0f402
suho <sirius0991@gmail.com>
2025-01-08 16:46:34 +0900

    feat: Create store functionality implemented store 생성 기능을 구현 -store컨트롤러에서 storeRequestDto와 오너 객체를 받아서 createStore 메서드를 호출 -Store서비스에서 StoreRequestDto를 이용해 store 객체를 생성하고 저장하는 로직을 추가

--- src/main/java/team7/delivery/controller/StoreController.java
+++ src/main/java/team7/delivery/controller/StoreController.java
@@ -1,4 +1,25 @@
 package team7.delivery.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team7.delivery.dto.store.StoreRequestDto;
+import team7.delivery.dto.store.StoreResponseDto;
+import team7.delivery.entity.Owner;
+import team7.delivery.service.StoreService;
+
+@RestController
+@RequestMapping("/api/stores")
+@RequiredArgsConstructor
 public class StoreController {
+
+    private final StoreService storeService;
+
+    @PostMapping
+    public ResponseEntity<StoreResponseDto> createStore(@Valid @RequestBody StoreRequestDto requestDto,
+                                                        @RequestAttribute Owner owner) {
+        StoreResponseDto responseDto = storeService.createStore(requestDto, owner);
+        return ResponseEntity.ok(responseDto);
+    }
 }
--- src/main/java/team7/delivery/service/StoreService.java
+++ src/main/java/team7/delivery/service/StoreService.java
@@ -1,4 +1,25 @@
 package team7.delivery.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team7.delivery.dto.store.StoreRequestDto;
+import team7.delivery.dto.store.StoreResponseDto;
+import team7.delivery.entity.Owner;
+import team7.delivery.entity.Store;
+import team7.delivery.repository.StoreRepository;
+
+@Service
+@RequiredArgsConstructor
 public class StoreService {
+
+    private final StoreRepository storeRepository;
+
+    public StoreResponseDto createStore(StoreRequestDto requestDto, Owner owner) {
+
+        Store store = Store.of(requestDto.getStoreName(), requestDto.getMinPrice(), requestDto.getOpenTime(), requestDto.getCloseTime(), owner);
+
+        Store savedStore = storeRepository.save(store);
+
+        return StoreResponseDto.of(savedStore);
+    }
 }
