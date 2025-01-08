1da1d10f273fa7a8984f8329613d809ea1bb7ca5
suho <sirius0991@gmail.com>
2025-01-08 20:23:14 +0900

    refactor: Code review 코드리뷰

--- src/main/java/team7/delivery/dto/store/StoreRequestDto.java
+++ src/main/java/team7/delivery/dto/store/StoreRequestDto.java
@@ -1,9 +1,10 @@
 package team7.delivery.dto.store;
 
 import lombok.Getter;
-import team7.delivery.entity.Store;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class StoreRequestDto {
 
     private final Long ownerId;
@@ -12,11 +13,4 @@ public class StoreRequestDto {
     private final String openTime;
     private final String closeTime;
 
-    public StoreRequestDto(Long ownerId, String storeName, Integer minPrice, String openTime, String closeTime) {
-        this.ownerId = ownerId;
-        this.storeName = storeName;
-        this.minPrice = minPrice;
-        this.openTime = openTime;
-        this.closeTime = closeTime;
-    }
 }
