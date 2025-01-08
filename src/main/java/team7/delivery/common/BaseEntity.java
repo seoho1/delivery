2602c07fd9f1fff576aa7aabe108bb7832a2d1c2
zabastian <qlgh10gh@gmail.com>
2025-01-08 10:16:54 +0900

    Changes by flashback

---
+++ src/main/java/team7/delivery/common/BaseEntity.java
@@ -0,0 +1,25 @@
+package team7.delivery.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+//    @Temporal(TemporalType.TIMESTAMP) 생략가능
+    private LocalDateTime created_At;
+
+    @LastModifiedDate
+    private LocalDateTime updated_at;
+}
