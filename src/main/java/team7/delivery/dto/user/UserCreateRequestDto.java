103eaf3a9883457d5517595a08cf35a70f452ad0
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-01-09 09:30:12 +0900

    feat: add role field of dto dto 에도 role 필드 추가함

--- src/main/java/team7/delivery/dto/user/UserCreateRequestDto.java
+++ src/main/java/team7/delivery/dto/user/UserCreateRequestDto.java
@@ -1,10 +1,12 @@
 package team7.delivery.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import team7.delivery.dto.auth.Role;
 
 @RequiredArgsConstructor
 @Getter
@@ -22,5 +24,8 @@ public class UserCreateRequestDto {
     @Size(min = 8, message = "비밀번호는 최소 8글자 이상이어야 합니다.")
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private final String password;
+
+    @NotNull
+    private final Role role;
 
 }
