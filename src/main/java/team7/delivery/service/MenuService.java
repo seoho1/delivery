f3e1cc0b998229dac045e1d7285bf1115f58f508
zabastian <qlgh10gh@gmail.com>
2025-01-10 12:03:03 +0900

    feat : add menu crud
    메뉴 crud를 dev에 병합

--- src/main/java/team7/delivery/service/MenuService.java
+++ src/main/java/team7/delivery/service/MenuService.java
@@ -1,20 +1,14 @@
 package team7.delivery.service;
 
-//import com.example.menu.dto.MenuDto;
-//import com.example.menu.dto.MenuRequestDto;
-//import com.example.menu.entity.Menu;
-//import com.example.menu.entity.Store;
-//import com.example.menu.repository.MenuRepository;
-//import com.example.menu.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
-//import org.apache.catalina.Store;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import team7.delivery.dto.menu.MenuDto;
 import team7.delivery.dto.menu.MenuRequestDto;
 import team7.delivery.entity.Menu;
 import team7.delivery.entity.Store;
-import team7.delivery.exception.StoreException;
+import team7.delivery.exception.ApiException;
+import team7.delivery.exception.ExceptionUtil;
+import team7.delivery.exception.util.ErrorMessage;
 import team7.delivery.repository.MenuRepository;
 import team7.delivery.repository.StoreRepository;
 
@@ -25,7 +19,7 @@ public class MenuService {
     private final StoreRepository storeRepository;
 
     public MenuDto createMenu(MenuRequestDto request){
-        Store store = storeRepository.findById(request.getStoreId()).orElseThrow(() -> new StoreException("가게가 없습니다.", HttpStatus.NOT_FOUND));
+        Store store = storeRepository.findById(request.getStoreId()).orElseThrow(() -> ExceptionUtil.throwErrorMessage(ErrorMessage.ENTITY_NOT_FOUND, ApiException.class));
         Menu menu = Menu.of(request,store);
         menuRepository.save(menu);
         return menuDto(menu);
@@ -33,13 +27,13 @@ public class MenuService {
 
     public MenuDto getMenu(Long menuId){
         Menu menu = menuRepository.findById(menuId)
-                .orElseThrow(() -> new StoreException("메뉴가 없습니다.",HttpStatus.NOT_FOUND));
+                .orElseThrow(() ->ExceptionUtil.throwErrorMessage(ErrorMessage.ENTITY_NOT_FOUND, ApiException.class));
         return menuDto(menu);
     }
 
     public MenuDto updateMenu(Long menusId,MenuRequestDto request){
         Menu menu = menuRepository.findById(menusId)
-                .orElseThrow(()-> new StoreException("수정 메뉴가 없습니다.",HttpStatus.NOT_FOUND));
+                .orElseThrow(()-> ExceptionUtil.throwErrorMessage(ErrorMessage.ENTITY_NOT_FOUND, ApiException.class));
         Menu.off(menusId,request);
         menuRepository.save(menu);
         return menuDto(menu);
@@ -47,7 +41,7 @@ public class MenuService {
     }
     public void deleteMenu(Long menusId){
         Menu menu = menuRepository.findActiveMenuById(menusId)
-                .orElseThrow(() -> new StoreException("삭제 메뉴가 없습니다.", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> ExceptionUtil.throwErrorMessage(ErrorMessage.ENTITY_NOT_FOUND, ApiException.class));
         menu.delete();
         menuRepository.save(menu);
     }
