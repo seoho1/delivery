773858e34fc8bdc8d5c8d8fb488d6d19e3841f81
suho <sirius0991@gmail.com>
2025-01-10 17:02:49 +0900

    refactor: repair type 오픈시간과 마감시간을 정확한 시간 포맷관리를 위해 변경

--- src/main/java/team7/delivery/dto/store/StoreResponseDto.java
+++ src/main/java/team7/delivery/dto/store/StoreResponseDto.java
@@ -3,14 +3,16 @@ package team7.delivery.dto.store;
 import lombok.Getter;
 import team7.delivery.entity.Store;
 
+import java.time.LocalTime;
+
 @Getter
 public class StoreResponseDto {
 
     private final Long id;
     private final String storeName;
     private final Integer minPrice;
-    private final String openTime;
-    private final String closeTime;
+    private final LocalTime openTime;
+    private final LocalTime closeTime;
     private final boolean isDeleted;
 
     public StoreResponseDto(Store store) {
