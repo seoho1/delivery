5c0587d3877e5f1e3efc1f50693e7382c5514f3e
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-01-10 09:41:20 +0900

    Revert "Feature/user api"

--- src/main/java/team7/delivery/entity/User.java
+++ src/main/java/team7/delivery/entity/User.java
@@ -1,6 +1,5 @@
 package team7.delivery.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,9 +18,6 @@ public class User extends BaseEntity {
 
     private String password;
 
-    @Column(nullable = false)
-    private Boolean isDeleted = false;
-
     protected User(){
 
     }
@@ -33,9 +29,5 @@ public class User extends BaseEntity {
 
     public static User of(String email, String password) {
         return new User(email, password);
-    }
-
-    public void deactivate() {
-        this.isDeleted = true;
     }
 }
