8e3afefcf11bc02d07c8673fdb83defcb07e8d2f
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-01-12 18:06:58 +0900

    fix: fix the error 컴파일 오류 해결함

--- src/main/java/team7/delivery/exception/util/ErrorMessage.java
+++ src/main/java/team7/delivery/exception/util/ErrorMessage.java
@@ -13,7 +13,7 @@ public enum ErrorMessage {
     VALID_ERROR("입력 값이 잘못되었습니다.", HttpStatus.BAD_REQUEST),
     INVALID_STATUS("상태값이 잘못되었습니다.", HttpStatus.UNAUTHORIZED),
     MINIMUM_ORDER_AMOUNT_NOT_MET("최소 주문 금액을 충족하지 못했습니다.", HttpStatus.UNAUTHORIZED),
-    STORE_CLOSED("현재 가게는 영업 시간이 아닙니다.", HttpStatus.UNAUTHORIZED);
+    STORE_CLOSED("현재 가게는 영업 시간이 아닙니다.", HttpStatus.UNAUTHORIZED),
     STORE_NOT_FOUND("가게를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     OWNER_FORBIDDEN("사장님만 가게를 만들 수 있습니다.", HttpStatus.FORBIDDEN),
     MAX_STORE_LIMIT("사장님은 가게를 최대 3개까지만 운영할 수 있습니다.", HttpStatus.BAD_REQUEST),
