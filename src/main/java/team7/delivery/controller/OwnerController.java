4bf678c425740f89e371528e16dd787548aa4018
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-01-12 17:09:38 +0900

    feat: add OwnerController OwnerController 클래스 작성함

--- src/main/java/team7/delivery/controller/OwnerController.java
+++ src/main/java/team7/delivery/controller/OwnerController.java
@@ -1,4 +1,28 @@
 package team7.delivery.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team7.delivery.entity.Owner;
+import team7.delivery.service.OwnerService;
+
+@RestController
+@RequestMapping("/owners")
+@RequiredArgsConstructor
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @PostMapping("/signup-owner")
+    public ResponseEntity<OwneCreateResponseDto> createOwner(@Valid @RequestBody OwneCreateResponseDto dto){
+
+        ownerService.createOwner(dto.getEmail(), dto.getPassword());
+
+        return new ResponseEntity<>(owner, HttpStatus.CREATED);
+    }
 }
